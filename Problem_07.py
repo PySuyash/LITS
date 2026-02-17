8c00b46e0122bce2b6d8ca250366e4406343802d
PySuyash <unknownup31@gmail.com>
2026-02-17 19:33:33 +0530

    Problem_07.py

---
+++ Problem_07.py
@@ -0,0 +1,8 @@
+# Write a program to check wheather a character is lowercase or uppercase
+
+char = input("Character : ")
+
+if char.isupper():
+    print(f"'{char}' is uppercase")
+else:
+    print(f"'{char}' is lowercase")
