62cfb349ad8fd9192f9e545e7a92e0ef60fff1a6
PySuyash <unknownup31@gmail.com>
2026-02-17 19:21:20 +0530

    Problem_03.py

---
+++ Problem_03.py
@@ -0,0 +1,9 @@
+# Write a program to find the largest of three numbers
+
+num1 = int(input("First number : "))
+num2 = int(input("Second number : "))
+num3 = int(input("Third number : "))
+
+largest = max(num1, num2, num3)
+
+print(f"Largest : {largest}")
