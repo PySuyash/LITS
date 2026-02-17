510f2a77bd940adac817a980cd441e11bff47510
PySuyash <unknownup31@gmail.com>
2026-02-17 19:24:25 +0530

    Problem_04.py

---
+++ Problem_04.py
@@ -0,0 +1,8 @@
+# Write a program to find the remainder when a number is divided by another number
+
+num1 = int(input("Number 1 : "))
+num2 = int(input("Number 2 : "))
+
+rem = num1 % num2
+
+print(f"Remainder : {rem}")
