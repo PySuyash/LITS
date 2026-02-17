efa9391ca5e7f211e2ab2ff90c81d86e8b03a80d
PySuyash <unknownup31@gmail.com>
2026-02-17 19:18:47 +0530

    Problem_02

---
+++ Problem_02.py
@@ -0,0 +1,7 @@
+# Write a program to convert celsius to fahrenheit
+
+temp_celsius = float(input("Temperature (in celsius) : "))
+
+temp_fahrenheit = (temp_celsius * (9/5)) + 32
+
+print(f"Themepratue (in fahrenheit) : {temp_fahrenheit}")
